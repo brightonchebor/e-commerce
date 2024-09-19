459e790ce353f14e4e5153f033aa34e94d73f013
brightonchebor <cheborbrighton805@gmailcom>
2024-09-19 22:22:24 +0300

    cn

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -125,5 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 import os
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
--- myapp/models.py
+++ myapp/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     slug = models.SlugField(max_length=255)
     in_stock = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
