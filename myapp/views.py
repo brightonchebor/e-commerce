cbfd16e4d08bbb9a45f66c737fd69e40826911c5
brightonchebor <cheborbrighton805@gmailcom>
2024-09-24 07:22:10 +0300

    cn

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -11,3 +11,7 @@ def categories(request):
     return {
         'categories': Category.objects.all()
     }
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'products/detail.html', {'product':product})
