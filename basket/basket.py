95154ec5da1764f54a61a0f04236cb2b8e50dbfe
brightonchebor <cheborbrighton805@gmailcom>
2024-10-06 13:29:26 +0300

    cn

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,13 @@
 class Basket():
 
+    """
+    A base Basket class, providing some default behaviors that
+    can be inherited or overrided, as necessary.
+    """
+
     def __init__(self, request):
-        pass
+        self.session = request.session
+        basket = self.session.get('skey')
+        if 'skey' not in request.session:
+            basket = self.session['skey'] = {'number': 12626536531}
+        self.basket = basket
