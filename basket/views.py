530aca4346f9c75ba82c90edca4b8cd09da5090e
brightonchebor <cheborbrighton805@gmailcom>
2024-10-06 14:01:43 +0300

    cn

--- basket/views.py
+++ basket/views.py
@@ -5,16 +5,17 @@ from django.http import JsonResponse
 
 # Create your views here.
 def basket_summary(request):
-    return render(request, 'basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'basket/summary.html', {'basket': basket})
 
 def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
-        product_qty = int(request.POST.get('productqty'))
+        # product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product, qty=product_qty)
+        # basket.add(product=product, qty=product_qty)
 
-        basketqty = basket.__len__()
-        response = JsonResponse({'qty': basketqty})
+        # basketqty = basket.__len__()
+        response = JsonResponse({'test': 'data'})
         return response
